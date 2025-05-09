6861ab853361150b074d673d937e706b1e4176f6
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-09 15:37:01 +0530

    changes in syntax

--- app.js
+++ app.js
@@ -2,21 +2,18 @@ const http=require('http');
 const server=http.createServer((req,res)=>{
     console.log('server is created!')
     res.setHeader('Content-Type','text/html')
-    if(req.url==='/home'){
-        res.statusCode=200;
+    if (req.url === '/home') {
+        res.statusCode = 200;
         res.end('<h1>Welcome home</h1>');
-    }else{
-        if(req.url==='/about'){
-            res.statusCode=200;
-            res.end('<h1>Welcome to About Us</h1>')
-        }if(req.url==='/node'){
-            res.statusCode=200;
-            res.end('<h1>Welcome to my Node Us</h1>')
-        }
-        else{
-            res.statusCode=404;
-            res.end('<h1>Page not found</h1>')
-        }
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.end('<h1>Welcome to About Us</h1>');
+    } else if (req.url === '/node') {
+        res.statusCode = 200;
+        res.end('<h1>Welcome to my Node Us</h1>');
+    } else {
+        res.statusCode = 404;
+        res.end('<h1>Page not found</h1>');
     }
 });
 const port=3000;
