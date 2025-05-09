b1196753760b25b5ed209d79662d40924d6e57f5
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-05-09 15:32:10 +0530

    changes in url

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ const http=require('http');
 const server=http.createServer((req,res)=>{
     console.log('server is created!')
     res.setHeader('Content-Type','text/html')
-    if(req.url==='/'){
+    if(req.url==='/home'){
         res.statusCode=200;
-        res.end('<h1>Hello World</h1>');
+        res.end('<h1>Welcome home</h1>');
     }else{
-        if(req.url==='/pizza'){
+        if(req.url==='/about'){
             res.statusCode=200;
-            res.end('<h1>This is yoyr pizza</h1>')
-        }else{
-            res.statusCode=400;
+            res.end('<h1>Welcome to About Us</h1>')
+        }if(req.url==='/node'){
+            res.statusCode=200;
+            res.end('<h1>Welcome to my Node Us</h1>')
+        }
+        else{
+            res.statusCode=404;
             res.end('<h1>Page not found</h1>')
         }
     }
